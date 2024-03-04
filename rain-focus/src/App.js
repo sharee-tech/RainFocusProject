6f5ee9efca297491dfed1173e94b53692107c276
sharee-tech <mail.sharee@gmail.com>
2024-03-03 20:55:30 -0600

    added useState and cleaned code

--- rain-focus/src/App.js
+++ rain-focus/src/App.js
@@ -1,7 +1,42 @@
 // import "./App.css";
+import { useState } from "react";
 import "./App.scss";
 
+const origData = [
+  {
+    id: 1,
+    icon: "logic-arrow.png",
+    title: "Attendee Registration",
+    content: "Start by creating a general registration workflow",
+  },
+  {
+    id: 2,
+    icon: "logic-arrow.png",
+    title: "Attendee Registration",
+    content: "Start by creating a general registration workflow",
+  },
+  {
+    id: 3,
+    icon: "logic-arrow.png",
+    title: "Attendee Registration",
+    content: "Start by creating a general registration workflow",
+  },
+];
+
 function App() {
+  const [cards, setCards] = useState(origData);
+
+  function handleClick() {
+    setCards([
+      ...cards,
+      {
+        id: cards.length + 1,
+        icon: "logic-arrow.png",
+        title: "Attendee Registration",
+        content: "Start by creating a general registration workflow",
+      },
+    ]);
+  }
   return (
     <>
       <Header />
@@ -16,7 +51,7 @@ function App() {
         <hr></hr>
         <section className="person-portal">
           <div>
-            <img src="images/person-portal.png" alt="Person Portal Image" />
+            <img src="images/person-portal.png" alt="Person Portal" />
           </div>
           <div>
             <h3>Org Settings</h3>
@@ -49,26 +84,13 @@ function App() {
             <b>Step: 2 </b>Build registration workflows.
           </h5>
           <div className="card-tile tile--2">
-            <CardTile
-              icon="logic-arrow.png"
-              title="Attendee Registration"
-              body="Start by creating a general registration workflow"
-            />
-            <CardTile
-              icon="logic-arrow.png"
-              title="Attendee Registration"
-              body="Start by creating a general registration workflow"
-            />
-            <CardTile
-              icon="logic-arrow.png"
-              title="Attendee Registration"
-              body="Start by creating a general registration workflow"
-            />
+            <StepTwo cards={cards} />
             <CardTile
               style1="center"
               icon="plus-icon.png"
               body="Start by creating a general registration workflow"
               style2="no-flex"
+              handleClick={handleClick}
             />
           </div>
         </section>
@@ -161,18 +183,43 @@ function BaseTile({ title, body }) {
     </>
   );
 }
-function CardTile({ icon, title, body, style1, style2 }) {
+function CardTile({ icon, title, body, style1, style2, handleClick }) {
   return (
     <>
       <div className={style1}>
         <div className={style2 ? `card-title ${style2}` : "card-title"}>
-          <img src={`images/${icon}`} width="20px" />
+          {style2 ? (
+            <img
+              src={`images/${icon}`}
+              alt="icon"
+              width="20px"
+              onClick={handleClick}
+              style={{ cursor: "pointer" }}
+            />
+          ) : (
+            <img src={`images/${icon}`} alt="icon" width="20px" />
+          )}
           <h6>{title}</h6>
         </div>
         <div>
           <p>{body}</p>
         </div>
       </div>
+    </>
+  );
+}
+
+function StepTwo({ cards }) {
+  return (
+    <>
+      {cards.map((card) => (
+        <CardTile
+          key={card.id}
+          icon={card.icon}
+          title={card.title}
+          body={card.content}
+        />
+      ))}
     </>
   );
 }
