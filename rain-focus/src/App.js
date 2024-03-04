1a530e7219de061ffce0cbe830b8d7d5db4058ed
sharee-tech <mail.sharee@gmail.com>
2024-03-03 18:42:46 -0600

    cleaned up <head> code and styling

--- rain-focus/src/App.js
+++ rain-focus/src/App.js
@@ -4,73 +4,29 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="styles.css" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-          crossorigin="anonymous"
-        />
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Inter&display=swap')
-        </style>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap')
-        </style>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap')
-        </style>
-        <title>RainFocus Summit</title>
-      </head>
-      <body>
-        {/* <Header>First Header</Header>
-        <Header>Second Header</Header> */}
-        <Header />
-        <main>
-          <section>
-            <h2>Event setup guide</h2>
-            <h4>
-              See the available list of modules below. We suggest that you start
-              with the attendee module.
-            </h4>
-          </section>
-          <section>
-            <hr></hr>
-            <div style={{ display: "inline-block", verticalAlign: "middle" }}>
-              <img
-                src="images/person-portal.png"
-                alt="Person Portal Image"
-                style={{ width: "35px", display: "inline-block" }}
-              />
-            </div>
-            <div
-              style={{
-                display: "inline-block",
-                verticalAlign: "middle",
-                marginLeft: "20px",
-              }}
-            >
-              <h3
-                style={{
-                  display: "inline-block",
-                  verticalAlign: "middle",
-                  margin: 0,
-                }}
-              >
-                Org Settings
-              </h3>
-            </div>
-          </section>
+      <Header />
+      <main>
+        <section className="header-two">
+          <h2>Event setup guide</h2>
+          <h4>
+            See the available list of modules below. We suggest that you start
+            with the attendee module.
+          </h4>
+        </section>
+        <hr></hr>
+        <section className="person-portal">
+          <div>
+            <img src="images/person-portal.png" alt="Person Portal Image" />
+          </div>
+          <div>
+            <h3>Org Settings</h3>
+          </div>
+        </section>
+        <section className="step-one">
           <h5>
             <b>Step: 1 </b>Base settings.
           </h5>
-          <div className="step-one">
+          <div className="tiles">
             <BaseTile
               title="General"
               body="Define Attendee types & attributes"
@@ -87,6 +43,8 @@ function App() {
             />
             <BaseTile title="" body="" />
           </div>
+        </section>
+        <section className="step-two">
           <h5>
             <b>Step: 2 </b>Build registration workflows.
           </h5>
@@ -113,7 +71,8 @@ function App() {
               style2="no-flex"
             />
           </div>
-
+        </section>
+        <section className="step-three">
           <h5>
             <b>Step: 3 </b>Design attendee experiences.
           </h5>
@@ -124,44 +83,48 @@ function App() {
               body="Manage the portal that attendees will see after they’ve register for your event."
             />
           </div>
+        </section>
+        <section className="step-four">
           <h5>
             <b>Step: 4 </b>Build your exhibitor platform.
           </h5>
-          <div className="step-four-1">
-            <div className="card-tile-2">
-              <CardTile
-                icon="computer-white.png"
-                title="Exhibitor Portal"
-                body="Manage the portal that attendees will see after they’ve register for your event."
-              />
-              <CardTile
-                icon="computer-white.png"
-                title="Exhibitor List"
-                body="Manage the portal that attendees will see after they’ve register for your event."
-              />
+          <div className="step-four-tiles">
+            <div className="row-one">
+              <div className="--tile">
+                <CardTile
+                  icon="computer-white.png"
+                  title="Exhibitor Portal"
+                  body="Manage the portal that attendees will see after they’ve register for your event."
+                />
+                <CardTile
+                  icon="computer-white.png"
+                  title="Exhibitor List"
+                  body="Manage the portal that attendees will see after they’ve register for your event."
+                />
+              </div>
+            </div>
+            <div className="row-two">
+              <div className="--tile">
+                <CardTile
+                  icon="computer-white.png"
+                  title="Exhibitor Logos"
+                  body="Manage the portal that attendees will see after they’ve register for your event."
+                />
+                <CardTile
+                  icon="computer-white.png"
+                  title="Exhibitor Tasks"
+                  body="Manage the portal that attendees will see after they’ve register for your event."
+                />
+                <CardTile
+                  icon="computer-white.png"
+                  title="Exhibitor Forms"
+                  body="Manage the portal that attendees will see after they’ve register for your event."
+                />
+              </div>
             </div>
           </div>
-          <div className="step-four-2">
-            <div className="card-tile-3">
-              <CardTile
-                icon="computer-white.png"
-                title="Exhibitor Logos"
-                body="Manage the portal that attendees will see after they’ve register for your event."
-              />
-              <CardTile
-                icon="computer-white.png"
-                title="Exhibitor Tasks"
-                body="Manage the portal that attendees will see after they’ve register for your event."
-              />
-              <CardTile
-                icon="computer-white.png"
-                title="Exhibitor Forms"
-                body="Manage the portal that attendees will see after they’ve register for your event."
-              />
-            </div>
-          </div>
-        </main>
-      </body>
+        </section>
+      </main>
     </>
   );
 }
@@ -169,23 +132,17 @@ function App() {
 function Header() {
   return (
     <>
-      <div className="header-row">
-        <div className="left-sidebar">
-          <img
-            src="images/rain-focus-logo.png"
-            alt="RainFocus Logo"
-            style={{ width: "89px" }}
-          />
+      <div className="header">
+        <div className="event-brand">
+          <img src="images/rain-focus-logo.png" alt="RainFocus Logo" />
+          <div className="details">
+            <h1>RainFocus Summit</h1>
+            <h4>
+              December 15th<br></br>Lehi, Utah
+            </h4>
+          </div>
         </div>
-        <div className="main-content">
-          <h1>RainFocus Summit</h1>
-          <h4>
-            December 15th<br></br>Lehi, Utah
-          </h4>
-        </div>
-        <div className="right-sidebar">
-          <button>Edit Event</button>
-        </div>
+        <button>Edit Event</button>
       </div>
     </>
   );
@@ -193,7 +150,7 @@ function Header() {
 function BaseTile({ title, body }) {
   return (
     <>
-      <div>
+      <div className="base-tile">
         <div>
           <h6>{title}</h6>
         </div>
@@ -218,83 +175,6 @@ function CardTile({ icon, title, body, style1, style2 }) {
       </div>
     </>
   );
-}
-
-function StepOne() {
-  return (
-    <>
-      <div className="step-one">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-      </div>
-    </>
-  );
-}
-
-// function StepTwo() {
-//   return (
-//     <>
-//       <p>
-//         <b>Step: 2 </b>Build registration workflows.
-//       </p>
-//       <div className="step-two">
-//         <div>1</div>
-//         <div>2</div>
-//         <div>3</div>
-//         <div>4</div>
-//       </div>
-//     </>
-//   );
-// }
-
-function StepThree() {
-  return (
-    <>
-      <h5>
-        <b>Step: 3 </b>Design attendee experiences.
-      </h5>
-      <div className="step-three">
-        <div>Clara is so happy to go outside and play</div>
-      </div>
-    </>
-  );
-}
-function StepFour() {
-  return (
-    <>
-      <h5>
-        <b>Step: 4 </b>Build your exhibitor platform.
-      </h5>
-      <div className="step-four">
-        <div class="item item--1">
-          <div>Red</div>
-        </div>
-        <div class="item item--2">
-          <div>Orange</div>
-        </div>
-        <div class="item item--3">
-          <div>Yellow</div>
-        </div>
-        <div class="item item--4">
-          <div>Green</div>
-        </div>
-        <div class="item item--5">
-          <div>Blue</div>
-        </div>
-      </div>
-    </>
-  );
-}
-{
-  /* function Tile({ icon, title, content }) {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-} */
 }
 
 export default App;
