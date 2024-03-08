d4f4cedd3847ea186d9306088a016f8e6211b370
sharee-tech <mail.sharee@gmail.com>
2024-03-08 12:05:48 -0600

    refactored component tree

--- rain-focus/src/App.js
+++ rain-focus/src/App.js
@@ -40,109 +40,12 @@ function App() {
     <>
       <Header />
       <main>
-        <section className="header-two">
-          <h2>Event setup guide</h2>
-          <h4>
-            See the available list of modules below. We suggest that you start
-            with the attendee module.
-          </h4>
-        </section>
-        <hr></hr>
-        <section className="person-portal">
-          <div>
-            <img src="images/person-portal.png" alt="Person Portal" />
-          </div>
-          <div>
-            <h3>Org Settings</h3>
-          </div>
-        </section>
-        <section className="step-one">
-          <h5>
-            <b>Step: 1 </b>Base settings.
-          </h5>
-          <div className="tiles">
-            <BaseTile
-              title="General"
-              body="Define Attendee types & attributes"
-            />
-            <BaseTile
-              title="Title"
-              body="Description that explains the value goes here. Description that
-              explains the value goes here."
-            />
-            <BaseTile
-              title="Title"
-              body="Description that explains the value goes here. Description that
-              explains the value goes here."
-            />
-            <BaseTile title="" body="" />
-          </div>
-        </section>
-        <section className="step-two">
-          <h5>
-            <b>Step: 2 </b>Build registration workflows.
-          </h5>
-          <div className="card-tile tile--2">
-            <StepTwo cards={cards} />
-            <CardTilePlus
-              icon="plus-icon.png"
-              body="Add Registration Workflow"
-              handleClick={handleClick}
-            />
-          </div>
-        </section>
-        <section className="step-three">
-          <h5>
-            <b>Step: 3 </b>Design attendee experiences.
-          </h5>
-          <div className="card-tile tile--3">
-            <CardTile
-              icon="computer.png"
-              title="Attendee Portal"
-              body="Manage the portal that attendees will see after they’ve register for your event."
-            />
-          </div>
-        </section>
-        <section className="step-four">
-          <h5>
-            <b>Step: 4 </b>Build your exhibitor platform.
-          </h5>
-          <div className="step-four-tiles">
-            <div className="row-one">
-              <div className="--tile">
-                <CardTile
-                  icon="computer-white.png"
-                  title="Exhibitor Portal"
-                  body="Manage the portal that attendees will see after they’ve register for your event."
-                />
-                <CardTile
-                  icon="computer-white.png"
-                  title="Exhibitor List"
-                  body="Manage the portal that attendees will see after they’ve register for your event."
-                />
-              </div>
-            </div>
-            <div className="row-two">
-              <div className="--tile">
-                <CardTile
-                  icon="computer-white.png"
-                  title="Exhibitor Logos"
-                  body="Manage the portal that attendees will see after they’ve register for your event."
-                />
-                <CardTile
-                  icon="computer-white.png"
-                  title="Exhibitor Tasks"
-                  body="Manage the portal that attendees will see after they’ve register for your event."
-                />
-                <CardTile
-                  icon="computer-white.png"
-                  title="Exhibitor Forms"
-                  body="Manage the portal that attendees will see after they’ve register for your event."
-                />
-              </div>
-            </div>
-          </div>
-        </section>
+        <HeaderTwo />
+        <PersonPortal />
+        <StepOne />
+        <StepTwo cards={cards} handleClick={handleClick} />
+        <StepThree />
+        <StepFour />
       </main>
     </>
   );
@@ -150,22 +53,46 @@ function App() {
 
 function Header() {
   return (
-    <>
-      <div className="header">
-        <div className="event-brand">
-          <img src="images/rain-focus-logo.png" alt="RainFocus Logo" />
-          <div className="details">
-            <h1>RainFocus Summit</h1>
-            <h4>
-              December 15th<br></br>Lehi, Utah
-            </h4>
-          </div>
+    <header className="header">
+      <div className="event-brand">
+        <img src="images/rain-focus-logo.png" alt="RainFocus Logo" />
+        <div className="details">
+          <h1>RainFocus Summit</h1>
+          <h4>
+            December 15th<br></br>Lehi, Utah
+          </h4>
         </div>
-        <button onClick={() => alert("Start editing event now...")}>
-          Edit Event
-        </button>
       </div>
-    </>
+      <button onClick={() => alert("Start editing event now...")}>
+        Edit Event
+      </button>
+    </header>
+  );
+}
+
+function HeaderTwo() {
+  return (
+    <section className="header-two">
+      <h2>Event setup guide</h2>
+      <h4>
+        See the available list of modules below. We suggest that you start with
+        the attendee module.
+      </h4>
+      <hr style={{ marginTop: "24px" }}></hr>
+    </section>
+  );
+}
+
+function PersonPortal() {
+  return (
+    <section className="person-portal">
+      <div>
+        <img src="images/person-portal.png" alt="Person Portal" />
+      </div>
+      <div>
+        <h3>Org Settings</h3>
+      </div>
+    </section>
   );
 }
 
@@ -217,7 +144,49 @@ function CardTilePlus({ icon, body, handleClick }) {
   );
 }
 
-function StepTwo({ cards }) {
+function StepOne() {
+  return (
+    <section className="step-one">
+      <h5>
+        <b>Step: 1 </b>Base settings.
+      </h5>
+      <div className="tiles">
+        <BaseTile title="General" body="Define Attendee types & attributes" />
+        <BaseTile
+          title="Title"
+          body="Description that explains the value goes here. Description that
+      explains the value goes here."
+        />
+        <BaseTile
+          title="Title"
+          body="Description that explains the value goes here. Description that
+      explains the value goes here."
+        />
+        <BaseTile title="" body="" />
+      </div>
+    </section>
+  );
+}
+
+function StepTwo({ cards, handleClick }) {
+  return (
+    <section className="step-two">
+      <h5>
+        <b>Step: 2 </b>Build registration workflows.
+      </h5>
+      <div className="card-tile tile--2">
+        <StepTwoCards cards={cards} />
+        <CardTilePlus
+          icon="plus-icon.png"
+          body="Add Registration Workflow"
+          handleClick={handleClick}
+        />
+      </div>
+    </section>
+  );
+}
+
+function StepTwoCards({ cards }) {
   return (
     <>
       {cards.map((card) => (
@@ -229,6 +198,68 @@ function StepTwo({ cards }) {
         />
       ))}
     </>
+  );
+}
+
+function StepThree() {
+  return (
+    <section className="step-three">
+      <h5>
+        <b>Step: 3 </b>Design attendee experiences.
+      </h5>
+      <div className="card-tile tile--3">
+        <CardTile
+          icon="computer.png"
+          title="Attendee Portal"
+          body="Manage the portal that attendees will see after they’ve register for your event."
+        />
+      </div>
+    </section>
+  );
+}
+
+function StepFour() {
+  return (
+    <section className="step-four">
+      <h5>
+        <b>Step: 4 </b>Build your exhibitor platform.
+      </h5>
+      <div className="step-four-tiles">
+        <div className="row-one">
+          <div className="--tile">
+            <CardTile
+              icon="computer-white.png"
+              title="Exhibitor Portal"
+              body="Manage the portal that attendees will see after they’ve register for your event."
+            />
+            <CardTile
+              icon="computer-white.png"
+              title="Exhibitor List"
+              body="Manage the portal that attendees will see after they’ve register for your event."
+            />
+          </div>
+        </div>
+        <div className="row-two">
+          <div className="--tile">
+            <CardTile
+              icon="computer-white.png"
+              title="Exhibitor Logos"
+              body="Manage the portal that attendees will see after they’ve register for your event."
+            />
+            <CardTile
+              icon="computer-white.png"
+              title="Exhibitor Tasks"
+              body="Manage the portal that attendees will see after they’ve register for your event."
+            />
+            <CardTile
+              icon="computer-white.png"
+              title="Exhibitor Forms"
+              body="Manage the portal that attendees will see after they’ve register for your event."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
